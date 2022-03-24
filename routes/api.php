bb7f1fa0a2b89850fa32cb911af2391320b678e2
BinPhan <binalwayssmile@gmail.com>
2022-03-24 10:35:21 +0700

    Add user files

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::post('login', 'UserAPIController@login');
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('users', App\Http\Controllers\API\UserAPIController::class);
